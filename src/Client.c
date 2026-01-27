2274432ee4effa7aee6e15c9d76e7ac1616dc82f
codeleaded <gnu.c.reaper@gmail.com>
2026-01-27 01:25:53 +0100

    updated

--- src/Client.c
+++ src/Client.c
@@ -7,44 +7,73 @@
 
 #define SIGNAL_MSG  (SIGNAL_START + 0)
 Client c;
+TextBox text;
 TextBox tb;
+TextBox nametb;
+String buffer;
 
-void Client_Proc_Connect(Vector* signalhandlers,Client* c,void* data,int size){
-    printf("Client_Connect: %d\n",c->sockfd);
+void Client_Proc_Connect(void* parent,Signal sig,Client* c,void* data,int size){
+    String_Appendf(&text.In.Buffer,"[%d]: Connect!\n",c->sockfd);
     Client_Signal_Send(c,SIGNAL_CONNECT,NULL,0);
 }
-void Client_Proc_Disconnect(Vector* signalhandlers,Client* c,void* data,int size){
-    printf("Client_Disconnect: %d\n",c->sockfd);
+void Client_Proc_Disconnect(void* parent,Signal sig,Client* c,void* data,int size){
+    String_Appendf(&text.In.Buffer,"[%d]: Disconnect!\n",c->sockfd);
     Client_Signal_Send(c,SIGNAL_DISCONNECT,NULL,0);
 }
-void Client_Proc_Msg(Vector* signalhandlers,Client* c,void* data,int size){
-    printf("Client_Msg: %d -> '%s' (%d)\n",c->sockfd,(char*)data,size);
+void Client_Proc_Msg(void* parent,Signal sig,Client* c,void* data,int size){
+    String_Appendf(&text.In.Buffer,"%s\n",(char*)data);
 }
 
 void Setup(AlxWindow* w){
 	c = Client_Make("5000","192.168.2.99",(SignalHandler[]){
-        SignalHandler_New(SIGNAL_CONNECT,(void (*)(void*,void*,void*,int))Client_Proc_Connect),
-        SignalHandler_New(SIGNAL_DISCONNECT,(void (*)(void*,void*,void*,int))Client_Proc_Disconnect),
-        SignalHandler_New(SIGNAL_MSG,(void (*)(void*,void*,void*,int))Client_Proc_Msg),
+        SignalHandler_New(SIGNAL_CONNECT,(void (*)(void*,Signal,void*,void*,int))Client_Proc_Connect),
+        SignalHandler_New(SIGNAL_DISCONNECT,(void (*)(void*,Signal,void*,void*,int))Client_Proc_Disconnect),
+        SignalHandler_New(SIGNAL_MSG,(void (*)(void*,Signal,void*,void*,int))Client_Proc_Msg),
         SignalHandler_Null()
     });
 
-	tb = TextBox_New(
+	text = TextBox_New(
+		Input_New(30,INPUT_MAXLENGTH),
+		(Rect){ 0.0f,0.0f,w->Width,w->Height * 9 / 10 },
+		ALXFONT_PATHS_BLOCKY,
+		32,
+		32,
+		GRAY
+	);
+    tb = TextBox_New(
 		Input_New(100,1),
-		(Rect){ 0.0f,w->Height * 9 / 10,w->Width,w->Height / 10 },
+		(Rect){ 0.0f,w->Height * 9 / 10,w->Width * 7 / 10,w->Height / 10 },
 		ALXFONT_PATHS_BLOCKY,
 		32,
 		32,
 		BLACK
 	);
+    nametb = TextBox_New(
+		Input_New(16,1),
+		(Rect){ w->Width * 7 / 10,w->Height * 9 / 10,w->Width * 3 / 10,w->Height / 10 },
+		ALXFONT_PATHS_BLOCKY,
+		32,
+		32,
+		DARK_GRAY
+	);
 }
 void Update(AlxWindow* w){
+	//TextBox_Update(&text,window.Strokes,GetMouse());
 	TextBox_Update(&tb,window.Strokes,GetMouse());
-        
+	TextBox_Update(&nametb,window.Strokes,GetMouse());
+
+    text.r = (Rect){ 0.0f,0.0f,w->Width,w->Height * 9 / 10 };
+    tb.r = (Rect){ 0.0f,w->Height * 9 / 10,w->Width * 7 / 10,w->Height / 10 };
+    nametb.r = (Rect){ w->Width * 7 / 10,w->Height * 9 / 10,w->Width * 3 / 10,w->Height / 10 };
+
+    
     Client_Update(&c);
     Client_DoAll(&c,NULL);
 
 	if(Stroke(ALX_KEY_ENTER).PRESSED){
+        String_Add(&tb.In.Buffer,"]: ",0);
+        String_AddString(&tb.In.Buffer,&nametb.In.Buffer,0);
+        String_Add(&tb.In.Buffer,"[",0);
 		CStr out = String_CStr(&tb.In.Buffer);
 
         Client_Signal_Send(&c,SIGNAL_MSG,out,tb.In.Buffer.size + 1);
@@ -55,10 +84,14 @@ void Update(AlxWindow* w){
 
 	Clear(WHITE);
 
+    TextBox_Render(WINDOW_STD_ARGS,&text);
 	TextBox_Render(WINDOW_STD_ARGS,&tb);
+	TextBox_Render(WINDOW_STD_ARGS,&nametb);
 }
 void Delete(AlxWindow* w){
+    TextBox_Free(&text);
     TextBox_Free(&tb);
+    TextBox_Free(&nametb);
     Client_Free(&c);
 }
 
--- src/Server.c
+++ src/Server.c
@@ -1,66 +1,43 @@
-#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
-#include "/home/codeleaded/System/Static/Library/TransformedView.h"
-#include "/home/codeleaded/System/Static/Library/Networking.h"
 #include "/home/codeleaded/System/Static/Library/CStr.h"
 #include "/home/codeleaded/System/Static/Library/Networking.h"
 
 
 #define SIGNAL_MSG  (SIGNAL_START + 0)
-Server s;
-TextBox tb;
 
-void Server_Proc_Connect(Vector* signalhandlers,Client* c,void* data,int size){
+void Server_Proc_Connect(Server* s,Signal sig,Client* c,void* data,int size){
     printf("Server_Connect: %d\n",c->sockfd);
 }
-void Server_Proc_Disconnect(Vector* signalhandlers,Client* c,void* data,int size){
+void Server_Proc_Disconnect(Server* s,Signal sig,Client* c,void* data,int size){
     printf("Server_Disconnect: %d\n",c->sockfd);
 }
-void Server_Proc_Msg(Vector* signalhandlers,Client* c,void* data,int size){
+void Server_Proc_Msg(Server* s,Signal sig,Client* c,void* data,int size){
     printf("Server_Msg: %d -> '%s' (%d)\n",c->sockfd,(char*)data,size);
-}
-
-void Setup(AlxWindow* w){
-	s = Server_Make(5000,10,(SignalHandler[]){
-        SignalHandler_New(SIGNAL_CONNECT,(void (*)(void*,void*,void*,int))Server_Proc_Connect),
-        SignalHandler_New(SIGNAL_DISCONNECT,(void (*)(void*,void*,void*,int))Server_Proc_Disconnect),
-        SignalHandler_New(SIGNAL_MSG,(void (*)(void*,void*,void*,int))Server_Proc_Msg),
-        SignalHandler_Null()
-    });
-
-    Server_Start(&s);
-
-	tb = TextBox_New(
-		Input_New(100,1),
-		(Rect){ 0.0f,w->Height * 9 / 10,w->Width,w->Height / 10 },
-		ALXFONT_PATHS_BLOCKY,
-		32,
-		32,
-		BLACK
-	);
-}
-void Update(AlxWindow* w){
-	TextBox_Update(&tb,window.Strokes,GetMouse());
-
-	if(Stroke(ALX_KEY_ENTER).PRESSED){
-		CStr out = String_CStr(&tb.In.Buffer);
-
-        Server_Signal_Send(&s,SIGNAL_MSG,out,tb.In.Buffer.size + 1);
-        CStr_Free(&out);
-
-        String_Clear(&tb.In.Buffer);
-	}
-
-	Clear(WHITE);
-
-	TextBox_Render(WINDOW_STD_ARGS,&tb);
-}
-void Delete(AlxWindow* w){
-    TextBox_Free(&tb);
-    Server_Free(&s);
+    //Server_Signal_SendExcept(s,(Client*[]){ c },1U,SIGNAL_MSG,data,size);
+    Server_Signal_Send(s,SIGNAL_MSG,data,size);
 }
 
 int main(){
-    if(Create("Text:Server",1000,1000,1,1,Setup,Update,Delete))
-        Start();
+    Server s = Server_Make(5000,10,(SignalHandler[]){
+        SignalHandler_New(SIGNAL_CONNECT,(void (*)(void*,Signal,void*,void*,int))Server_Proc_Connect),
+        SignalHandler_New(SIGNAL_DISCONNECT,(void (*)(void*,Signal,void*,void*,int))Server_Proc_Disconnect),
+        SignalHandler_New(SIGNAL_MSG,(void (*)(void*,Signal,void*,void*,int))Server_Proc_Msg),
+        SignalHandler_Null()
+    });
+
+    Server_Start_C(&s);
+    Server_Start_R(&s);
+
+    while(1){
+        printf("[Server]: Cmd -> ");
+        fflush(stdout);
+        
+        char buffer[1024];
+        int size = scanf("%1023s",buffer);
+        
+        if(CStr_Cmp(buffer,"exit"))
+            break;
+    }
+
+    Server_Free(&s);
     return 0;
 }
